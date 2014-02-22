ea37a70e1159e200ce1af071cbd73e1da64eb833
AlexxHansson <alexxhansson@gmail.com>
2014-02-22 13:22:30 +0100

    API init

--- api/app.js
+++ api/app.js
@@ -1,1 +1,20 @@
-console.log('Be gone foul woman!');
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+app.get('/', function (req, res) {
+    'use strict';
+    //Serve application
+    res.sendfile(__dirname + '/gui/app/index.html');
+    console.log('Be gone foul woman!');
+});
+/*
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
+*/
